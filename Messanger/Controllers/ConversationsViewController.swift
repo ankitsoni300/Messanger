ec3aeb5761d68026e9bf1460d0e1dcae2949caf2
Ankit Soni <soni.ankit1@indiamart.com>
2020-09-05 21:17:45 +0530

    wrap up new files

--- Messanger/Controllers/ConversationsViewController.swift
+++ Messanger/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messanger
 //
 //  Created by imart on 05/09/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
