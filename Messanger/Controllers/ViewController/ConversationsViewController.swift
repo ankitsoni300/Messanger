ad1dff382b11721d1157fd46324b48e6849153ee
Ankit Soni <soni.ankit1@indiamart.com>
2020-09-28 16:44:05 +0530

    add plist

--- Messanger/Controllers/ViewController/ConversationsViewController.swift
+++ Messanger/Controllers/ViewController/ConversationsViewController.swift
@@ -15,6 +15,7 @@ class ConversationsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //
     }
 
     override func viewDidAppear(_ animated: Bool) {
